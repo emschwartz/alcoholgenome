3236a8b84e50d253608c67e007287d9f2dbf0d6e
Evan Schwartz <evan.mark.schwartz@gmail.com>
2013-05-10 14:22:18 -0400

    changed the way the beer options are added to the search bar to make it a lot faster

--- web.js
+++ web.js
@@ -9,34 +9,51 @@ var app = express();
 app.use(express.bodyParser());
 
 
-var name_list = [];
+var beer_list = [];
 
 
 app.get('/get_beer_list', function(req, res) {
-	if (name_list.length == 0) {
+	if (beer_list.length == 0) {
 	pg.connect(connectionString, function (err, client) {
-		var query = client.query("select distinct name, brewery from alcoholgenome order by name asc");
-
+		var query = client.query("select distinct * from alcoholgenome order by name asc");
+		var index = 0;
 		query.on('row', function (row) {
-			name_list.push(row.name + " - " + row.brewery);
+			beer_list.push({"id":row.name.replace('&amp;', '&') + " - " + row.brewery.replace('&amp;', '&'), "text": row.name.replace('&amp;', '&') + " - " + row.brewery.replace('&amp;', '&')});
 		});
 		
 		query.on('end', function() {
-			res.send(name_list);
+			// console.log(beer_list);
+			res.send(beer_list);
 			// console.log("ending client");
 			client.end();
 		});
 	});
 } else {
-	res.send(name_list);
+	res.send(beer_list);
 }
 });
 
 
 app.post('/search', function(req, res) {
+	// var search_beer_nums = req.body.search_items;
+	// if (search_beer_nums.length > 0) {
+	// 	var search_beer_names = [];
+	// 	for (var b = 0; b < search_beer_nums.length; b++) {
+	// 		search_beer_names.push(beer_list[search_beer_nums[b]].text);
+	// 	}
 	var search_beer_names = req.body.search_items;
+	if (search_beer_names.indexOf(',') != -1) {
+		search_beer_names = search_beer_names.split(',');
+	} else {
+		search_beer_names = [search_beer_names];
+	}
+	// console.log(search_beer_names);
 	if (search_beer_names.length > 0) {
 		pg.connect(connectionString, function (err, client) {
+			for (var b = 0; b < search_beer_names.length; b++) {
+				search_beer_names[b] = search_beer_names[b].replace('&', '&amp;');
+			}
+			// console.log(search_beer_names);
 			beerSearcher.search(search_beer_names, client, res);
 		});
 	} else {
